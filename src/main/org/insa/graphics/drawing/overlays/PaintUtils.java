bc70c4267d29e528c3c7544208d58beb8e4eb0eb
Holt59 <capelle.mikael@gmail.com>
2018-03-03 12:48:55 +0100

    Update polyline auto scaling width.

--- src/main/org/insa/graphics/drawing/overlays/PaintUtils.java
+++ src/main/org/insa/graphics/drawing/overlays/PaintUtils.java
@@ -22,10 +22,16 @@ public class PaintUtils {
      * @param width
      * @return
      */
-    public static int getStrokeWidth(int width, byte zoomLevel) {
-        int mul = 2;
-        if (zoomLevel < 8) {
+    public static float getStrokeWidth(int width, byte zoomLevel) {
+        float mul = 1;
+        if (zoomLevel < 6) {
             mul = 1;
+        }
+        else if (zoomLevel < 10) {
+            mul += (zoomLevel - 5) * 0.5;
+        }
+        else if (zoomLevel < 13) {
+            mul = 3.5f;
         }
         else {
             mul += 2 * (zoomLevel - 8) / 3;
